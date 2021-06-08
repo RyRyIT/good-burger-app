5d27fd275166ea798dd8cef3bddb03c4f25e40a9
Ryan Lackey <lackeyr13@gmail.com>
2021-06-08 19:37:03 -0400

    herokupushh

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
         host: 'localhost',
         port: 3306,
         user: 'root',
-        password: "MerRy042020!",
+        password: "MerRy042020!", 
         database: 'burgers_db',
       }); 
 }
